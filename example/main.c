04b978b85e0f045df33f1b2f027e8d4b4d85ddff
sicris <sicris.embay@gmail.com>
2023-04-01 11:43:04 +0800

    + Added TI peripheral files + Added littlefs submodule

--- example/main.c
+++ example/main.c
@@ -3,12 +3,13 @@
  */
 
 #include <xdc/std.h>
-
 #include <xdc/runtime/System.h>
-
+#include <xdc/runtime/Types.h>
 #include <ti/sysbios/BIOS.h>
-
 #include <ti/sysbios/knl/Task.h>
+#include <ti/sysbios/knl/Clock.h>
+#include "DSP2833x_Device.h"
+#include "driver_def.h"
 
 /*
  *  ======== taskFxn ========
@@ -22,11 +23,146 @@ Void taskFxn(UArg a0, UArg a1)
     System_printf("exit taskFxn()\n");
 }
 
+
+extern unsigned int RamfuncsLoadStart;
+extern unsigned int RamfuncsLoadEnd;
+extern unsigned int RamfuncsRunStart;
+extern unsigned int RamConstLoadStart;
+extern unsigned int RamConstLoadEnd;
+extern unsigned int RamConstRunStart;
+
+
+#pragma CODE_SECTION(InitFlashWaitState, "ramfuncs");
+static void InitFlashWaitState(void)
+{
+    EALLOW;
+
+    //
+    // Enable Flash Pipeline mode to improve performance
+    // of code executed from Flash.
+    //
+    FlashRegs.FOPT.bit.ENPIPE = 1;
+
+    //
+    //                CAUTION
+    // Minimum waitstates required for the flash operating
+    // at a given CPU rate must be characterized by TI.
+    // Refer to the datasheet for the latest information.
+    //
+    //
+    // Set the Paged Waitstate for the Flash
+    //
+    FlashRegs.FBANKWAIT.bit.PAGEWAIT = 5;
+
+    //
+    // Set the Random Waitstate for the Flash
+    //
+    FlashRegs.FBANKWAIT.bit.RANDWAIT = 5;
+
+    //
+    // Set the Waitstate for the OTP
+    //
+    FlashRegs.FOTPWAIT.bit.OTPWAIT = 8;
+
+    //
+    //                CAUTION
+    // ONLY THE DEFAULT VALUE FOR THESE 2 REGISTERS SHOULD BE USED
+    //
+    FlashRegs.FSTDBYWAIT.bit.STDBYWAIT = 0x01FF;
+    FlashRegs.FACTIVEWAIT.bit.ACTIVEWAIT = 0x01FF;
+
+    EDIS;
+
+    //
+    // Force a pipeline flush to ensure that the write to
+    // the last register configured occurs before returning.
+    //
+    asm(" RPT #7 || NOP");
+}
+
+
+static void configure_core_pll(UInt16 val)
+{
+    /* Make sure the PLL is not running in limp mode */
+    if (SysCtrlRegs.PLLSTS.bit.MCLKSTS != 0)
+    {
+       /*
+        * Missing external clock has been detected
+        * Replace this line with a call to an appropriate
+        * SystemShutdown(); function.
+        */
+       asm("        ESTOP0");
+    }
+
+    /* Change the PLLCR */
+    if (SysCtrlRegs.PLLCR.bit.DIV != val)
+    {
+
+       EALLOW;
+       /* Before setting PLLCR turn off missing clock detect logic */
+       SysCtrlRegs.PLLSTS.bit.MCLKOFF = 1;
+       SysCtrlRegs.PLLCR.bit.DIV = val;
+       EDIS;
+
+       /*
+        * Wait for PLL to lock.
+        * During this time the CPU will switch to OSCCLK/2 until
+        * the PLL is stable.  Once the PLL is stable the CPU will
+        *
+        * Wait for the PLL lock bit to be set.
+        */
+       while(SysCtrlRegs.PLLSTS.bit.PLLLOCKS == 0) {
+           /*
+            * Note: The watchdog should be fed within
+            * the loop via ServiceDog().
+            */
+       }
+
+       EALLOW;
+       SysCtrlRegs.PLLSTS.bit.MCLKOFF = 0;
+       EDIS;
+     }
+}
+
+
+static void CLOCK_init(void)
+{
+    Types_FreqHz coreFreq;
+
+    /* Configure Core Frequency */
+    DINT;
+    IER = 0x0000;
+    IFR = 0x0000;
+
+    configure_core_pll(0xA);
+
+    InitFlashWaitState();
+
+    /* Update SYSBIOS Core Clock */
+    coreFreq.hi = 0;
+    coreFreq.lo = CONFIG_SYSTEM_FREQ_MHZ * 1000000U;
+    BIOS_setCpuFreq(&coreFreq);
+    Clock_tickReconfig();
+}
 /*
  *  ======== main ========
  */
 Int main()
-{ 
+{
+    /*
+     * Copy ramfuncs section
+     */
+    memcpy(&RamfuncsRunStart, &RamfuncsLoadStart,
+           &RamfuncsLoadEnd - &RamfuncsLoadStart);
+
+    /*
+     * Copy ramconsts section
+     */
+    memcpy(&RamConstRunStart, &RamConstLoadStart,
+           &RamConstLoadEnd - &RamConstLoadStart);
+
+    CLOCK_init();
+
     /*
      * use ROV->SysMin to view the characters in the circular buffer
      */
